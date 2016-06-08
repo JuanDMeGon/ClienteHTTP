5e16fc318ed551382595cbb8ac9649c6427cbe04
JuanDMeGon <contacto@juandmegon.com>
2016-06-08 18:19:22 -0500

    Preparando el Cliente para Realizar Peticiones Sobre HTTP y HTTPS

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -8,7 +8,18 @@ use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesResources;
 
+use GuzzleHttp\Client;
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, AuthorizesResources, DispatchesJobs, ValidatesRequests;
+
+    protected function realizarPeticion($metodo, $url, $parametros=[])
+    {
+    	$cliente = new Client(['curl' => [CURLOPT_CAINFO => base_path('resources/certs/cacert.pem')]]);
+
+    	$respuesta = $cliente->request($metodo, $url, $parametros);
+
+    	return $respuesta->getBody()->getContents();
+    }
 }
